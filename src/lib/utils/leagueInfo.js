5de75555b78dc14826a8f8877a26df20bc440c52
vaalibaba <69025012+vaalibaba@users.noreply.github.com>
2024-10-29 15:18:03 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -2,7 +2,7 @@
 export const leagueID = "1122160008088604672"; // your league ID
 export const leagueName = "Rumble in the Fumble"; // your league name
 export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
